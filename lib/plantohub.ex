8135b80354c35e012ca201a70a606d3d7c110992
Denis Grankin <liquidautumn@gmail.com>
2021-07-02 16:15:07 +0300

    Initial, yea, commit

---
+++ lib/plantohub.ex
@@ -0,0 +1,18 @@
+defmodule Plantohub do
+  @moduledoc """
+  Documentation for Plantohub.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Plantohub.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plantohub_test.exs
@@ -0,0 +1,8 @@
+defmodule PlantohubTest do
+  use ExUnit.Case
+  doctest Plantohub
+
+  test "greets the world" do
+    assert Plantohub.hello() == :world
+  end
+end
